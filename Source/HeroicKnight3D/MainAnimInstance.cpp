9108c409ee828d05a5cf64ba9e46aaabba713b93
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-06-20 11:05:59 +0300

    add player animation system

--- Source/HeroicKnight3D/MainAnimInstance.cpp
+++ Source/HeroicKnight3D/MainAnimInstance.cpp
@@ -3,3 +3,30 @@
 
 #include "MainAnimInstance.h"
 
+#include "GameFramework/PawnMovementComponent.h"
+
+void UMainAnimInstance::NativeInitializeAnimation()
+{
+	if (Pawn == nullptr)
+	{
+		Pawn = TryGetPawnOwner();
+	}
+}
+
+void UMainAnimInstance::UpdateAnimationProperties()
+{
+	if (Pawn == nullptr)
+	{
+		Pawn = TryGetPawnOwner();
+	}
+
+	if (Pawn)
+	{
+		FVector Speed = Pawn->GetVelocity();
+		FVector LateralSpeed = FVector(Speed.X, Speed.Y, 0.f);
+		MovementSpeed = LateralSpeed.Size();
+		UE_LOG(LogTemp, Warning, TEXT("%f"), MovementSpeed);
+		
+		bIsInAir = Pawn->GetMovementComponent()->IsFalling();
+	}
+}
--- Source/HeroicKnight3D/MainAnimInstance.h
+++ Source/HeroicKnight3D/MainAnimInstance.h
@@ -13,5 +13,20 @@ UCLASS()
 class HEROICKNIGHT3D_API UMainAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
+
+public:
 	
+	virtual void NativeInitializeAnimation() override;
+
+	UFUNCTION(BlueprintCallable, Category="Animation Properties")
+	void UpdateAnimationProperties();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Movement")
+	float MovementSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Movement")
+	bool bIsInAir;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Movement")
+	APawn* Pawn;
 };
