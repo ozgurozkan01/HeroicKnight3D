c1dcb05d39b5f940c43687f7065d738009a00708
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-06-24 12:15:29 +0300

    add some particle effect

---
+++ Source/HeroicKnight3D/SpawnVolume.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SpawnVolume.h"
+
+#include "Components/BoxComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+// Sets default values
+ASpawnVolume::ASpawnVolume()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	/*SpawnVolume = CreateDefaultSubobject<UBoxComponent>(TEXT("Spawn Volume"));*/
+	
+}
+
+// Called when the game starts or when spawned
+void ASpawnVolume::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASpawnVolume::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+/*
+FVector ASpawnVolume::GetSpawnPoint()
+{
+	FVector VolumeExtent = SpawnVolume->GetScaledBoxExtent();
+	FVector VolumeOrigin = SpawnVolume->GetComponentLocation();
+
+	/* This function is returning random point which is in SpawnVolume that we created #1#
+	FVector SpawnPoint = UKismetMathLibrary::RandomPointInBoundingBox(VolumeOrigin, VolumeExtent);
+
+	return SpawnPoint;
+}
+*/
+
---
+++ Source/HeroicKnight3D/SpawnVolume.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SpawnVolume.generated.h"
+
+class UBoxComponent;
+
+UCLASS()
+class HEROICKNIGHT3D_API ASpawnVolume : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASpawnVolume();
+
+	/*UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Spawn Volume")
+	UBoxComponent* SpawnVolume;*/
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	/*
+	UFUNCTION(BlueprintPure, Category="Spawn Volume")
+	FVector GetSpawnPoint();*/
+};
