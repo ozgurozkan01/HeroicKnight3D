f535f67a97bef6b67471d5934d281579421482ac
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-06-30 16:29:53 +0300

    add explosive item class

--- Source/HeroicKnight3D/Explosive.cpp
+++ Source/HeroicKnight3D/Explosive.cpp
@@ -3,3 +3,19 @@
 
 #include "Explosive.h"
 
+AExplosive::AExplosive()
+{
+	
+}
+
+void AExplosive::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnOverlapBegin(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+}
+
+void AExplosive::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	Super::OnOverlapEnd(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex);
+}
--- Source/HeroicKnight3D/Explosive.h
+++ Source/HeroicKnight3D/Explosive.h
@@ -13,5 +13,11 @@ UCLASS()
 class HEROICKNIGHT3D_API AExplosive : public AItem
 {
 	GENERATED_BODY()
+
+public:
+
+	AExplosive();
 	
+	virtual void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult) override;
+	virtual void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
 };
