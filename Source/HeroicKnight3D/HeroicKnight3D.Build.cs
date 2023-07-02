caa36d1230670d4637aa7d36a7333e59d32dfd2e
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-07-02 15:29:26 +0300

    add HUD modules

--- Source/HeroicKnight3D/HeroicKnight3D.Build.cs
+++ Source/HeroicKnight3D/HeroicKnight3D.Build.cs
@@ -10,7 +10,7 @@ public class HeroicKnight3D : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
