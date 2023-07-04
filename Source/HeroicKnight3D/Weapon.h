01dedb139aaf5cbee0b6e10c98f8b3f77c1abcdb
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-07-04 15:07:29 +0300

    add weapon assets

---
+++ Source/HeroicKnight3D/Weapon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item.h"
+#include "Weapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HEROICKNIGHT3D_API AWeapon : public AItem
+{
+	GENERATED_BODY()
+	
+};
