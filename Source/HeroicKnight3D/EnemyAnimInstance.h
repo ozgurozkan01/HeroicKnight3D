8b5c249a11fa0f6493cacbc7fef0b7788db38adc
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-07-07 17:48:37 +0300

    add enemy anim instance class

---
+++ Source/HeroicKnight3D/EnemyAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "EnemyAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HEROICKNIGHT3D_API UEnemyAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
