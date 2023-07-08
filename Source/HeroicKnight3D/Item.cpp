ce13c4fee773387bbb469bd35454eca25a228df8
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-07-09 00:01:44 +0300

    delete Text in OnOverlapEnd function

--- Source/HeroicKnight3D/Item.cpp
+++ Source/HeroicKnight3D/Item.cpp
@@ -58,7 +58,6 @@ void AItem::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* Oth
 void AItem::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
 	int32 OtherBodyIndex)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Super::OnOverlapEnd()"));
 }
 
 
