0cf4b7f45639921951992f837c17b81010c724de
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-07-12 23:42:45 +0300

    fix weapon attachment to socket

--- Source/HeroicKnight3D/Enemy.cpp
+++ Source/HeroicKnight3D/Enemy.cpp
@@ -29,7 +29,7 @@ AEnemy::AEnemy()
 	CombatSphere->SetSphereRadius(100.f);
 
 	CombatCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("CombatCollision"));
-	CombatCollision->SetupAttachment(GetMesh(), FName("EnemySocket"));
+	CombatCollision->SetupAttachment(GetMesh(), FName("EnemySocket")); 
 	
 	bOverlappingCombatSphere = false;
 	bHasValidTarget = false;
